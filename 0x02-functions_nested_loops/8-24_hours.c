3ed59c4eb4ceb8e002014f260f71a560d93735ca
Kevin Kamunyu <knkamunyu@gmail.com>
2022-09-20 01:09:47 +0300

    keep updating

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer, start
+ * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and resets mins
  * Return: 0
@@ -31,5 +30,3 @@ void jack_bauer(void)
 	minutes = 0;
 }
 }
-
-
