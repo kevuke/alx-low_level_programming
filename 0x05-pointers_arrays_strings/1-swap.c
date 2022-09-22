f9eb8e3c19af7472371c144974fecedb1e8eab8e
Kevin Kamunyu <knkamunyu@gmail.com>
2022-09-23 01:29:39 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,24 +1,16 @@
 #include "main.h"
 
 /**
- *_strncat - concatenate two strings but add inputted number of bytes
- *@dest: string to be appended upon
- *@src: string to be completed at end of dest
- *@n:integer parameter to compare index to
- *Return: returns new concatenated string
- */
+*swap_int - swaps two integers' values
+*@a: first integer
+*@b: second integer
+*Return: returns nothing
+*/
 
-char *_strncat(char *dest, char *src, int n)
+void swap_int(int *a, int *b)
 {
+	int tmp = *a;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
-
+	*a = *b;
+	*b = tmp;
 }
